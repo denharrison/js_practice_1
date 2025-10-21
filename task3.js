2378ed1fcab980b84098a00a2e58d514904c2351
Denis Harrison <denharrison797@gmail.com>
2025-10-21 20:03:48 +0300

    added task3

---
+++ task3.js
@@ -0,0 +1,15 @@
+const correctPassword = "1234" 
+let userPassword = prompt("Введите пожалуйста пароль")
+
+for (let i = 0; i !== 3; i++) {
+
+  if (userPassword === correctPassword) {
+    alert("Доступ разрешён");
+    break
+  } else {
+    alert("Доступ запрещён");
+    userPassword = prompt("Введите пожалуйста пароль");
+  }
+
+}
+
