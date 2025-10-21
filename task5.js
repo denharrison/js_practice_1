d9932b8551f641a3287b70f924e777511bdce3cf
Denis Harrison <denharrison797@gmail.com>
2025-10-21 20:30:01 +0300

    added task 5

---
+++ task5.js
@@ -0,0 +1,9 @@
+let userSymbol = prompt("Введите один символ")
+
+if (userSymbol > 0 && userSymbol < 9) {
+  alert("Цифра");
+} else if (userSymbol = /^[a-zA-Z]$/.test(userSymbol)) {
+  alert("Буква");
+} else {
+  alert("Неизвестный символ");
+}
