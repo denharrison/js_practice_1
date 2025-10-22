0a3e4d877a39aa04399ca0bfa48e83d82ef43fe6
Denis Harrison <denharrison797@gmail.com>
2025-10-22 15:54:47 +0300

    added 6 task

--- task5.js
+++ task5.js
@@ -2,7 +2,7 @@ let userSymbol = prompt("Введите один символ")
 
 if (userSymbol > 0 && userSymbol < 9) {
   alert("Цифра");
-} else if (userSymbol = /^[a-zA-Z]$/.test(userSymbol)) {
+} else if (userSymbol = /^[a-zA-Z]$/.test(userSymbol)) { // Регулярное выражение 
   alert("Буква");
 } else {
   alert("Неизвестный символ");
---
+++ task6.js
@@ -0,0 +1,11 @@
+let userNum = String(prompt('Введите пожалуйста число')) 
+let reverse = ''
+
+for (let i = userNum.length - 1 ; i >= 0 ; i--) {
+    
+    reverse += userNum[i]
+    
+}
+
+
+alert(reverse)
